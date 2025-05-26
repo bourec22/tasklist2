e477d7522982b9611a4377730703de9436baf89b
bourec22 <vaclav.vondrasek@wanted.solutions>
2025-05-26 13:08:35 +0200

    routes,controller, model

---
+++ app/Http/Controllers/Dashboards_Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Dashboards_Controller extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use app\http\Controllers\Dashboards_Controller;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -28,5 +28,8 @@ Route::get('/greet/{name}', function($name){
 Route ::get('/hallo', function(){
     return redirect('/hello');
 });
-//GEt
-//post 
+route::get('/dashboards', [Dashboards_Controller::class, 'index']) ->name('dashboards.index');
+route::post('/dashboards/create', [Dashboards_Controller::class, 'create']) ->name('dashboards.create');
+route::get('/dashboards/{id}', [Dashboards_Controller::class, 'detail']) ->name('dashboards.detail');
+route::put('/dashboards/{id}', [Dashboards_Controller::class,'edit']) ->name('dashboards.edit');
+route::delete('/dashboards/{id}', [Dashboards_Controller::class,'delete'])->name('dashboards.delete');
