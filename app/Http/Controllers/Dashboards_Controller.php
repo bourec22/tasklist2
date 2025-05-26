f8e7cc3b67787ae24c80c3bdf4a9e820246bb203
bourec22 <vaclav.vondrasek@wanted.solutions>
2025-05-26 13:35:39 +0200

    routes,controller, model2

--- app/Http/Controllers/Dashboards_Controller.php
+++ app/Http/Controllers/Dashboards_Controller.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Dashboard;
 use Illuminate\Http\Request;
 
 class Dashboards_Controller extends Controller
 {
-    //
+Public function index(){
+//route = /dashboard
+    return Dashboard::all();
+}
+
+Public function create(){
+//route = /dashboard/create
+
+}
+
+Public function detail(){
+//route = /dashboard/{id}
+
+}
+
+Public function edit(){
+//route = /dashboard/{id}
+
+}
+Public function delete(){
+//route = /dashboard/{id}
+
+}
 }
