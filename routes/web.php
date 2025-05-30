3b42268c35e8ec951d3c42003bf657e0066dd8b7
bourec22 <vasek@Vaclav--MacBook-Pro.local>
2025-05-30 12:56:07 +0200

    routes

--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'Main page';
-});
-
-Route ::get('/hello', function () {
-    return 'Hello';
-});
-
-Route::get('/greet/{name}', function($name){
-    return 'Hello ' . $name . '!';
-}) ;
-
-Route ::get('/hallo', function(){
-    return redirect('/hello');
-});
-//GEt
-//post 
+ 
+use App\Http\Controllers\Dashboards_Controller;
+route::get('/dashboards', [Dashboards_Controller::class, 'index']) ->name('dashboards.index');
+route::post('/dashboards/create', [Dashboards_Controller::class, 'create']) ->name('dashboards.create');
+route::get('/dashboards/{id}', [Dashboards_Controller::class, 'detail']) ->name('dashboards.detail');
+route::put('/dashboards/{id}', [Dashboards_Controller::class,'edit']) ->name('dashboards.edit');
+route::delete('/dashboards/{id}', [Dashboards_Controller::class,'delete'])->name('dashboards.delete');
