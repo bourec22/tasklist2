bfde6ed2a90c2bc907344e272aab7ebce1901e59
bourec22 <vasek@Vaclav--MacBook-Pro.local>
2025-06-04 17:24:33 +0200

    očesaný

--- app/Models/Dashboard.php
+++ app/Models/Dashboard.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Dashboard extends Model
 {
-     protected $guarded = ['id', 'name', ];
+     protected $guarded = ['id', 'name' ];
 
     use HasFactory;
 }
